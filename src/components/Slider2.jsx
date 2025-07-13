35d55bf4382181dd5ad6889d80ffac0733e613c5
Rindra <hei.rindra.2@gmail.com>
2025-07-13 22:55:43 +0300

    add teachers data with images

--- src/components/Slider2.jsx
+++ src/components/Slider2.jsx
@@ -9,3 +9,45 @@ import Yannick from "../assets/Yannick.jpg";
 import Tahina from "../assets/Tahina.png";
 import Lou from "../assets/Lou.jpg";
 
+const enseignants = [
+    {
+        nom: "Mirado RAFENOMAHENINTSOA",
+        description: "Data and Business Intelligence Specialist 5+",
+        image: Mirado,
+    },
+    {
+        nom: "Julien RAJERISON",
+        description: "Lead Développeur | Fondateur de l'association Techzara",
+        image: Julien,
+    },
+    {
+        nom: "Ryan ANDRIAMAHERY",
+        description: "Développeur back end | Cofondateur et Directeur des opérations de HEI",
+        image: Ryan,
+    },
+    {
+        nom: "Jean Aimé Maxa",
+        description: "Responsable technique cybersécurité chez CES  France Continental(Toulouse - France)",
+        image: Jean,
+    },
+    {
+        nom: "Parisson Ravalomanda",
+        description: "Ingénieur en informatique | Ingénieur logiciel chez Google (Londres - UK)",
+        image: Parison,
+    },
+    {
+        nom: "Yannick Raharijaona",
+        description: "Responsable technique ML chez Rocket Science (Ottawa - Canada)",
+        image: Yannick,
+    },
+    {
+        nom: "Dre Tahina Ralitera",
+        description: "Docteure-Ingénieure, CNRS France | Prix L'Oréal-UNESCO pour les Femmes en Science (2017)",
+        image: Tahina,
+    },
+    {
+        nom: "Dr Lou Maurica",
+        description: "Docteure-Ingénieure en informatique | Fondateur et Directeur pédagogique de HEI",
+        image: Lou,
+    },
+];
