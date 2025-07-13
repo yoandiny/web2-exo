aaabf46769e28619feefd0e866866fa5584251cb
Yoan <yoandiny14@gmail.com>
2025-07-13 07:11:09 +0300

    added about part and empty slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { Slider } from './components/Slider'
+import StudentImage from './assets/students.jpg'
 
 function App() {
 
@@ -9,7 +11,20 @@ function App() {
     <title>Accueil - HEI | Haute Ecole d'Informatique de Madagascar</title>
       <Navbar />
 
-      <h1>HEI</h1>
+      <Slider />
+
+      <section id='about'>
+        <div className="about-image-container">
+          <img src={StudentIma} alt="" />
+        </div>
+        <div className="about-text-container">
+          <h2 className='about-text'>A propos de nous</h2>
+        <h3>+3ans</h3>
+        <p>Depuis 3 ans, nous formons ceux qui décodent le futur, aux métiers de l’informatique. Nous sommes un groupe d’experts,
+           de professionnels et des passionnés de l’informatique de Madagascar et du monde entier.</p>
+        </div>
+
+      </section>
     </div>
   )
 }
---
+++ src/components/Slider.jsx
@@ -0,0 +1,22 @@
+import './styles/Slider.css';
+
+export const Slider = () => {
+    return (
+        <div className="slider">
+            <div className="slider-container">
+                <div className="slider-item">
+                    <img src="" alt=""/>
+                    
+                </div>
+                <div className="slider-item">
+                    <img src="" alt=""/>
+                    
+                </div>
+                <div className="slider-item">
+                    <img src="" alt=""/>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
