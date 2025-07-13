504feef19d786ce720c8223810c04813883f575f
Rindra <hei.rindra.2@gmail.com>
2025-07-13 22:54:34 +0300

    import slider images and hooks

---
+++ src/components/Slider2.jsx
@@ -0,0 +1,11 @@
+import { useEffect, useRef, useState } from "react";
+import "./styles/Slider2.css";
+import Mirado from "../assets/Mirado.jpg";
+import Julien from "../assets/Julien-150x150.jpg";
+import Ryan from "../assets/Ryan.jpg";
+import Jean from "../assets/Jean-Aime.jpg";
+import Parison from "../assets/Parison.png";
+import Yannick from "../assets/Yannick.jpg";
+import Tahina from "../assets/Tahina.png";
+import Lou from "../assets/Lou.jpg";
+
