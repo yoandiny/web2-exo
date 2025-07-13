bd99f49131feea8e731932f5a4de8d454f4bedcb
Rindra <hei.rindra.2@gmail.com>
2025-07-13 18:02:03 +0300

    update(slider): add MESupRES accreditation text to slide content, add buttons

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,9 +1,58 @@
 
-
+import heiBanner1 from "../assets/hei-banner-1.jpg";
+import heiBanner2 from "../assets/hei-banner-2.jpg";
+import heiBanner3 from "../assets/hei-banner-3.jpg";
 import "./styles/Slider.css";
 
+const slides = [
+    {
+        image: heiBanner1,
+        description: "Une infrastructure de qualité et un programme pédagogique qui répond au besoin du marché pour l'employabilité de nos étudiants.",
+    },
+    {
+        image: heiBanner2,
+        description: "«L’éducation est l’arme la plus puissante pour changer le monde» selon Nelson Mandela. «L’éducation dans l’informatique est une arme très puissante pour lutter contre la pauvreté à Madagascar» selon HEI. C’est notre mission.",
+    },
+    {
+        image: heiBanner3,
+        description: "Des entreprises partenaires et une équipe pédagogique et administrative qui travaille sans relâche pour la montée en compétences de nos étudiants.",
+    },
+];
+const extendedSlides = [...slides, slides[0]];
 
 
 export default function Slider() {
 
+
+    return (
+        <div className="slider">
+            <div
+                className={`slides-wrapper ${isTransitioning ? "transition" : ""}`}
+                style={{
+                    transform: `translateX(-${current * 100}%)`,
+                }}
+            >
+                {extendedSlides.map((slide, index) => (
+                    <div
+                        key={index}
+                        className="slide"
+                        style={{
+                            backgroundImage: `url(${slide.image})`,
+                        }}
+                    >
+                        <div className="slide-content">
+                            <h1>Haute Ecole d'Informatique</h1>
+                            <p>{slide.description}</p>
+                            <div className="button-container">
+                                <button className="main-btn">Inscrivez-vous ici</button>
+                                <button>Emploi du temps</button>
+                                <button>Programme pédagogique</button>
+                            </div>
+                            <p>Habilitation MESupRES, suivant l'arrêté n°31309/2023</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
 }
